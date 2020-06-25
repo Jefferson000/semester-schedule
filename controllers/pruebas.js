ec6ce4705f0d264fe994a166da89ac0a7a764783
Jefferson000 <jefersontorres000@gmail.com>
2020-06-25 15:18:51 -0600

    GetPruebas Route

--- controllers/pruebas.js
+++ controllers/pruebas.js
@@ -7,8 +7,11 @@ exports.GetPruebas = async (req) => {
     try {
         let pool = await sql.connect(sqlconfig);
         let result = await pool.request()
+            // .input('user', sql.VarChar(50), req.user)
+            .output('success', sql.Bit, 0)
             .execute('GetPruebas');
         sql.close();
+        console.log(result)
         return result;
     }
     catch (excepcion) {
--- routes/pruebas.js
+++ routes/pruebas.js
@@ -6,15 +6,32 @@ var HttpStatus = require('http-status-codes');
 var custom_consts = require('../config/constants');
 
 router.get('/GetPruebas', (req, res) => {
-    pruebasController.GetPruebas(req.body)
-        .then(result => {
-            if (result.output.success) res.status(HttpStatus.NO_CONTENT).json({});
-            else res.status(HttpStatus.BAD_REQUEST).json({ error: result.recordset[0].err});
-        })
-        .catch(err => {
-            console.log(res);
-            res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ error: err.message })
-        })
+    pruebasController.GetPruebas(req.query)
+    .then(result => {
+        res.status(HttpStatus.OK).json( {data: result.recordset });
+    })
+    .catch(err => {
+
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ error: err.message });
+    })
 });
 
+// pruebasController.GetPruebas(req.query)
+// .then(result => {
+//     res.status(HttpStatus.OK).json( {data: result.recordset });
+// })
+// .catch(err => {
+
+//     res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ error: err.message });
+// })
+//TODO ADD POST
+// pruebasController.GetPruebas(req.body)
+// .then(result => {
+//     if (result.output.success) res.status(HttpStatus.NO_CONTENT).json({});
+//     else res.status(HttpStatus.BAD_REQUEST).json({ error: result.recordset[0].err});
+// })
+// .catch(err => {
+//     console.log(res);
+//     res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ error: err.message })
+// })
 module.exports = router;
